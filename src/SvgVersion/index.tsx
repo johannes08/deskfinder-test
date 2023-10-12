97139713d58bb9ca7fd84bb23118c35588bb3615
Burak Onat <burak.onat@copitos.de>
2023-10-12 09:43:29 +0200

    zweites svg vom büro eingefügt

--- src/SvgVersion/index.tsx
+++ src/SvgVersion/index.tsx
@@ -1,35 +1,48 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { ReactSVG } from 'react-svg';
 import './index.css';
 
 const SvgVersion = () => {
     const [selectedDesk, setSelectedDesk] = useState<string>('');
+    const [svgInjected, setSvgInjected] = useState<boolean>(false);
+    const [svgVersion, setSvgVersion] = useState<boolean>(true);
+
+    const v1 = 'https://gist.githubusercontent.com/tburakonat/9ec4de19ae045dd95a60c33e0884ca3c/raw/5fc3e4f86206c5a7dbdb20ba87bb2ed649a2a453/office-layout.svg';
+    const v2 = 'https://gist.githubusercontent.com/tburakonat/fbc3eee91f36691354a56b2e2dd32c38/raw/bdbb318f9299dff6aba661f08326114cf8070af0/office-layout-2.svg';
+
 
     const handleClick = (e: any) => {
-        const target = e.target as any;
-        const classNames = Array.from(target.classList);
-
-        if (!classNames?.includes('desk')) return;
-
-        setSelectedDesk(target.id)
-
-        // alert(`Du hast ${target.id} ausgewählt`)
+        setSelectedDesk(e.target.id)
     }
 
+    useEffect(() => {
+        if (!svgInjected) return
+
+        const desks = document.querySelectorAll('.desk');
+        desks.forEach(desk => {
+            desk.addEventListener('click', handleClick);
+
+        })
+    }, [svgInjected])
+
     return (
-        <div className='svg-container'>
-            <ReactSVG
-                src='https://gist.githubusercontent.com/tburakonat/9ec4de19ae045dd95a60c33e0884ca3c/raw/5fc3e4f86206c5a7dbdb20ba87bb2ed649a2a453/office-layout.svg'
-                onClick={handleClick}
-            />
-            <dialog open={!!selectedDesk}>
-                <p>Willst du {selectedDesk} buchen?</p>
-                <form method="dialog" style={{display: 'flex', justifyContent: 'space-between'}}>
-                    <button onClick={() => setSelectedDesk('')}>Buchen</button>
-                    <button onClick={() => setSelectedDesk('')}>Abbrechen</button>
-                </form>
-            </dialog>
-        </div>
+        <>
+            <button onClick={() => setSvgVersion(!svgVersion)}>Switch SVG</button>
+            <div className='svg-container'>
+                <ReactSVG
+                    src={svgVersion ? v1 : v2}
+                    onClick={handleClick}
+                    afterInjection={() => setSvgInjected(true)}
+                />
+                <dialog open={!!selectedDesk}>
+                    <p>Willst du {selectedDesk} buchen?</p>
+                    <form method="dialog" style={{display: 'flex', justifyContent: 'space-between'}}>
+                        <button onClick={() => setSelectedDesk('')}>Buchen</button>
+                        <button onClick={() => setSelectedDesk('')}>Abbrechen</button>
+                    </form>
+                </dialog>
+            </div>
+        </>
     )
 
 }
